b8ec6e4bf87cecfc5c78d97608f89a42219114a8
Ro Lee <rolee0429@gmail.com>
2018-09-22 00:55:25 -0700

    clean up vulenerable modules

--- source/src/reducers/gameReducer.js
+++ source/src/reducers/gameReducer.js
@@ -4,9 +4,7 @@ const INITIAL_STATE = {
   score: 0
 };
 
-
 export default (state = INITIAL_STATE, action) => {
-  const payload = action.payload;
   switch (action.type) {
     case GamePlateActions.CREATE_BLOCK: {
       return Object.assign({...state}, { score: ++state.score });
